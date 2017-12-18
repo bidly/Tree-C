1c1c2782197d3a2fd40cf5be40061a89fae85786
Doruk Atalar <dorukatalar@gmail.com>
2017-12-19 02:51:26 +0300

    203-final Start.

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+
+
+    FILE *fp;
+    char *line = NULL, *line2 = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+
+    fp = fopen("read.txt", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+    int flag = 0;
+    int flag2 = 0;
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+
+        printf("%s", line);
+    }
+
 }
