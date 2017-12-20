f0c5f4521130d2c43a513b9b699eca256491fe39
Doruk Atalar <dorukatalar@gmail.com>
2017-12-20 03:09:44 +0300

    203-final Start.

--- main.c
+++ main.c
@@ -1,26 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mem.h>
 
 
 int main() {
 
 
 
-    FILE *fp;
+    FILE *fp, *fpSize;
     char *line = NULL, *line2 = NULL;
     size_t len = 0;
     ssize_t read;
 
 
-    fp = fopen("read.txt", "r");
+    fpSize = fopen("Input1.txt", "r");
+    fp = fopen("Input1.txt", "r");
     if (fp == NULL)
         exit(EXIT_FAILURE);
     int flag = 0;
     int flag2 = 0;
+    int sizeOfFirst = 0;
+
+
+    while ((read = getline(&line, &len, fpSize)) != -1) {
+        if(strlen(line) != 1)
+            sizeOfFirst++;
+    }
+    fclose(fpSize);
+
+    int nodes[sizeOfFirst][2];
+    int i = 0;
 
     while ((read = getline(&line, &len, fp)) != -1) {
-
-        printf("%s", line);
+        if(strlen(line) != 1)
+        {
+            char *token;
+            int j = 0;
+            token = strtok(line, " ");
+            while (token != NULL) {
+                nodes[i][j]= atoi(token);
+                token = strtok(NULL, " ");
+                j++;
+            }
+            i++;
+        }
     }
-
+    for(i = 0 ; i<sizeOfFirst; i++)
+        printf("%d ", nodes[i][1]);
 }
