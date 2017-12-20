ab7846358f7a61d4ba36793abca46b1dce1bad94
Doruk Atalar <dorukatalar@gmail.com>
2017-12-21 02:33:10 +0300

    203-final Start.

--- main.c
+++ main.c
@@ -2,8 +2,107 @@
 #include <stdlib.h>
 #include <mem.h>
 
+int size = 0;
+
+struct node {
+    int data;
+    int key;
+    int parent;
+    struct node *next;
+    struct node *prev;
+};
+struct node *head = NULL;
+
+void printList() { /*list printer function. */
+    struct node *ptr = head;
+    printf("\n[ ");
+
+    //start from the beginning
+
+    while (ptr != NULL) {
+        printf("(%d) ", ptr->data);
+        ptr = ptr->next;
+    }
+
+
+
+    printf(" ]");
+}
+
+void insertFirst(int data) { /*this function adds the numbers to the beggining of the linked list*/
+    //create a link
+    struct node *link = (struct node *) malloc(sizeof(struct node));
+
+    link->data = data;
+
+
+    //point it to old first node
+    link->next = head;
+
+    //point first to new first node
+    head = link;
+    size++;
+}
+
+
+struct node *delete(int key) { /*this func deletes a node with given key*/
+
+    //start from the first link
+    struct node *current = head;
+    struct node *previous = NULL;
+
+    //if list is empty
+    if (head == NULL) {
+        return NULL;
+    }
+
+    //navigate through list
+    while (current->key == key ) {
+
+        //if it is last node
+        if (current->next == NULL) {
+            return NULL;
+        } else {
+            //store reference to current link
+            previous = current;
+            //move to next link
+            current = current->next;
+        }
+    }
+
+    //found a match, update the link
+    if (current == head) {
+        //change first to point to next link
+        head = head->next;
+    } else {
+        //bypass the current link
+        previous->next = current->next;
+    }
+
+
+    return current;
+}
+
+
+void reverse(struct node **head_ref) { /*since my structure is first in last out, this func reverses the list.*/
+    struct node *prev = NULL;
+    struct node *current = *head_ref;
+    struct node *next;
+
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head_ref = prev;
+}
+
 
 int main() {
+
+
 
 
 
@@ -42,9 +141,11 @@ int main() {
                 token = strtok(NULL, " ");
                 j++;
             }
+            insertFirst(nodes[i][0]);
             i++;
         }
     }
-    for(i = 0 ; i<sizeOfFirst; i++)
-        printf("%d ", nodes[i][1]);
+
+    reverse(&head);
+    printList();
 }
